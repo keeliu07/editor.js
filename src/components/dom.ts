7b16fa4f8d9aeced574e2d8ad93277dd79fab13a
Kee Liu <wk.keekeeliu@gmail.com>
2021-04-12 23:50:03 +0800

    fix

--- src/components/dom.ts
+++ src/components/dom.ts
@@ -65,6 +65,8 @@ export default class Dom {
     for (const attrName in attributes) {
       if (Object.prototype.hasOwnProperty.call(attributes, attrName)) {
         el[attrName] = attributes[attrName];
+      }else{
+        el[attrName] = attributes[attrName];
       }
     }
 
